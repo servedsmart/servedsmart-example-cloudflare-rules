71e105783a8d99aae78335db53789f280bb287ad
Served Smart <195884188+servedsmart@users.noreply.github.com>
2025-05-25 04:14:34 +0200

    DEBUG: Remove debug statement

--- src/index.sh
+++ src/index.sh
@@ -316,7 +316,6 @@ if [[ -n "${RULESET_ID}" ]]; then
     RESPONSE="$(curl -s https://api.cloudflare.com/client/v4/zones/"${CLOUDFLARE_ZONE_ID}"/rulesets/"${RULESET_ID}" -X PUT -H "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" --json "${JSON_REDIRECT_RULESET}")"
     if ! jq -e ".success" <<<"${RESPONSE}" >/dev/null 2>&1; then
         echo "ERROR: Cloudflare API Request unsuccessful. PUT https://api.cloudflare.com/client/v4/zones/CLOUDFLARE_ZONE_ID/rulesets/RULESET_ID failed."
-        echo "DEBUG: ${RESPONSE}"
         exit 1
     fi
     echo "Cloudflare API Request successful. PUT https://api.cloudflare.com/client/v4/zones/CLOUDFLARE_ZONE_ID/rulesets/RULESET_ID succeeded."
